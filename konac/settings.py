2143259acee9d71a61a644bd0980d397d9a52ec3
tsetoguchi <Tao209@gmail.com>
2023-12-06 05:36:59 -0500

    deleted requests

--- konac/settings.py
+++ konac/settings.py
@@ -127,3 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
